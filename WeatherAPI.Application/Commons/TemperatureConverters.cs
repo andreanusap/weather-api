758ef07f578b4e0eebc3bb1bbd1f8e53c22a0c2a
Andreanus Agung Purnomo <andreanusagung.purnomo@mitrais.com>
2022-07-23 09:45:45 +0700

    Bug fixing & create common converter for temperatures

---
+++ WeatherAPI.Application/Commons/TemperatureConverters.cs
@@ -0,0 +1,24 @@
+﻿namespace WeatherAPI.Application.Commons;
+
+public static class TemperatureConverters
+{
+    public static double CelsiusToKelvin(double temperature)
+    {
+        return temperature + 273.15;
+    }
+
+    public static double FahrenheitToKelvin(double temperature)
+    {
+        return Math.Round((temperature - 32) * 5 / 9 + 273.15, 2);
+    }
+
+    public static double KelvinToCelcius(double temperature)
+    {
+        return Math.Round(temperature - 273.15, 2);
+    }
+
+    public static double KelvinToFahrenheit(double temperature)
+    {
+        return Math.Round(1.8 * (temperature - 273.15) + 32, 2);
+    }
+}
